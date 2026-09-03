5d5172b23b072721121f6c010b1499b2dbe6bfec
thai923711-debug <thai923711@gmail.com>
2026-09-03 07:37:20 +0700

    Update products.js

--- products.js
+++ products.js
@@ -18,7 +18,7 @@ const products = {
   "Sữa & đồ uống": [
     "Sữa Vinamilk không đường bịch", "Sữa Vinamilk có đường bịch", "Sữa đậu nành Fami bịch",
     "Sữa Milo hộp 180ml", "Sữa Milo hộp 110ml", "Sữa con bò 100% 110ml",
-    "Sữa con bò 100% 180ml", "Sữa Su Su Dâu Chai", "Cafe Sài Gòn", "Cafe Trần Quang",
+    "Sữa con bò 100% 180ml", "Sữa Su Su Dâu Chai", "Cafe Sài Gòn", "Cafe Trần Quang", "Cafe Phố Bịch",
     "Cafe Vinacafe", "Cafe Phố hộp", "Yến sào có đường", "Yến sào không đường", "Sữa ngôi sao giấy lớn", "Sữa ngôi sao giấy nhỏ"
   ],
   "Nước ngọt & bia": [
